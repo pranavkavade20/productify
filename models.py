447246a25c74237a77ab1154992dd22d3e2b4970
PranavGithubUser <pranavkavade9@gmail.com>
2025-12-15 20:13:00 +0530

    initial commit

---
+++ models.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+class Product(BaseModel):
+    id: int
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+
+# class Product:
+#     id: int
+#     name: str
+#     description: str
+#     price: float
+#     quantity: int
+
+#     def __init__(self, id:int, name:str, description: str, price:float, quantity: int ):
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.price = price
+#         self.quantity = quantity
